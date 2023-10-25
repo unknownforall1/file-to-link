a66e50295d142134f2c3b35440587cb85fafad39
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-26 01:57:30 +0530

    Update custom_dl.py

--- SiDoP/utils/custom_dl.py
+++ SiDoP/utils/custom_dl.py
@@ -1,14 +1,14 @@
 import math
 import asyncio
 import logging
-from Adarsh.vars import Var
+from SiDoP.vars import Var
 from typing import Dict, Union
-from Adarsh.bot import work_loads
+from SiDoP.bot import work_loads
 from pyrogram import Client, utils, raw
 from .file_properties import get_file_ids
 from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
-from Adarsh.server.exceptions import FIleNotFound
+from SiDoP.server.exceptions import FIleNotFound
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
 
