602d046dc1a9c1d3557799b21d3d4fbff6a6be41
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-26 01:59:08 +0530

    Update render_template.py

--- SiDoP/utils/render_template.py
+++ SiDoP/utils/render_template.py
@@ -1,8 +1,8 @@
-from Adarsh.vars import Var
-from Adarsh.bot import StreamBot
-from Adarsh.utils.human_readable import humanbytes
-from Adarsh.utils.file_properties import get_file_ids
-from Adarsh.server.exceptions import InvalidHash
+from SiDoP.vars import Var
+from SiDoP.bot import StreamBot
+from SiDoP.utils.human_readable import humanbytes
+from SiDoP.utils.file_properties import get_file_ids
+from SiDoP.server.exceptions import InvalidHash
 import urllib.parse
 import aiofiles
 import logging
