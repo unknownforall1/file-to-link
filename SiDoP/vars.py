5ee138bfa28f6a57c9d1215a5b1e66dc8fa632b3
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-09 11:42:18 +0530

    Update vars.py

--- SiDoP/vars.py
+++ SiDoP/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
+    API_ID = int(getenv('API_ID', '7988735'))
+    API_HASH = str(getenv('API_HASH', '8339b7684eb7f4653ed032d4828ebf89'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    name = str(getenv('name', 'Nobita-Stream-Bot'))
+    name = str(getenv('name', 'Movies-X-Store'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    WORKERS = int(getenv('WORKERS', '15'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002085145034'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", ""))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "120"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5792964753").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NobiDeveloperr'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Don_Owner'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
