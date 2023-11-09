367bed04862323b50c51663b6613e1c97750bc2b
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-09 14:22:32 +0530

    Update vars.py

--- SiDoP/vars.py
+++ SiDoP/vars.py
@@ -21,7 +21,6 @@ class Var(object):
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5792964753").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    MY_PASS = "MOVIESXSTORE"
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Don_Owner'))
     if 'DYNO' in environ:
         ON_HEROKU = True
