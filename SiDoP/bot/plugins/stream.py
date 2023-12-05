7b43258d75c88837852b1ae98af46f0d2827119a
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-05 10:24:55 +0530

    Update stream.py

--- SiDoP/bot/plugins/stream.py
+++ SiDoP/bot/plugins/stream.py
@@ -29,7 +29,6 @@ async def private_receive_handler(c: Client, m: Message):
         )
     if Var.UPDATES_CHANNEL != "None:
         try:
-        try:
             log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
             stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
             online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
